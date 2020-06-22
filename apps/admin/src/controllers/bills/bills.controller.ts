897c25d018a90a4ed361e7d5b59669f86c5c5069
the6thm0nth <the6thm0nth@outlook.com>
2020-06-22 20:08:42 +0900

    Add an endpoint to fetch private bills

--- apps/admin/src/controllers/bills/bills.controller.ts
+++ apps/admin/src/controllers/bills/bills.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, SetMetadata } from '@nestjs/common';
+import { Controller, Get, Query, SetMetadata, Request } from '@nestjs/common';
 import { Bill } from '@app/entities';
 import { BillsService } from '@app/modules';
 
@@ -17,6 +17,22 @@ export class BillsController {
       page,
       pageSize,
       { memberName },
+    );
+
+    return { bills, pageCount };
+  }
+
+  @Get('my')
+  async FindAllPrivate(
+    @Request() req,
+    @Query('page') page = 1,
+    @Query('pageSize') pageSize = 10,
+  ): Promise<{ bills: Bill[]; pageCount: number }> {
+    const userId = req.user.id;
+    const [bills, pageCount] = await this.billsService.findAllPrivate(
+      page,
+      pageSize,
+      { userId },
     );
 
     return { bills, pageCount };
